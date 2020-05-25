ad59cd05c6e70dd9aa78aa498bfb5e21d96c232d
Viraj Sinha <virajosinha@gmail.com>
2020-05-25 16:53:51 -0400

    implemented add[i] and load[i] and some helpful pretty printers

--- internal/vm.go
+++ internal/vm.go
@@ -1,6 +1,9 @@
 package internal
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type op int
 
@@ -27,6 +30,18 @@ const (
 	// TODO finish these
 )
 
+type opKind struct {
+	size uint
+}
+
+var (
+	unary  opKind = opKind{1} // NOP and HALT
+	rType         = opKind{4} // works on registers
+	riType        = opKind{4} // works on registers (with immediate values)
+	lType         = opKind{3} // works on addresses
+	liType        = opKind{3} // works on addresses (with immediate values)
+)
+
 // opExec is a map that points from an opcode to the operational parameters of the opcode.
 var opExec = map[op]struct {
 	name string
@@ -34,25 +49,54 @@ var opExec = map[op]struct {
 	// f is the function which will be called upon encountering this opcode
 	f func(*vm)
 
-	// size describes how many bytes in memory the function takes as arguments
-	size uint
+	// kind describes how many bytes in memory the function takes as arguments
+	kind opKind
 }{
-	nop:   {"nop", (*vm).nop, 1},     // nop           // nop
-	add:   {"add", (*vm).add, 4},     // add r1 r2 r3  // r1 = r1 + r2
-	addi:  {"addi", (*vm).addi, 4},   // add r1 r2 4   // r1 = r2 + 4
-	halt:  {"halt", (*vm).halt, 1},   // halt          // halt
-	loadi: {"loadi", (*vm).loadi, 3}, // loadi r1 5    // r1 = 5
-	load:  {"load", (*vm).load, 3},   // loadi r1 r2   // r1 = r2
-	sub:   {"sub", (*vm).sub, 4},     // sub r1 r2 r3  // r1 = r2 - r3
+	nop:   {"nop", (*vm).nop, unary},      // nop           // nop
+	add:   {"add", (*vm).add, rType},      // add r1 r2 r3  // r1 = r1 + r2
+	addi:  {"addi", (*vm).addi, riType},   // add r1 r2 4   // r1 = r2 + 4
+	halt:  {"halt", (*vm).halt, unary},    // halt          // halt
+	loadi: {"loadi", (*vm).loadi, liType}, // loadi r1 5    // r1 = 5
+	load:  {"load", (*vm).load, lType},    // loadi r1 r2   // r1 = r2
+	sub:   {"sub", (*vm).sub, rType},      // sub r1 r2 r3  // r1 = r2 - r3
 }
 
 type registers [15]int
+
 type memory []int
+
+func (m memory) String() string {
+	b := &strings.Builder{}
+
+	nopCount := 0
+	for i := 0; i < len(m); i++ {
+		o := op(m[i])
+
+		if o == nop {
+			nopCount++
+			continue
+		} else if nopCount != 0 {
+			fmt.Fprintf(b, "Found %d nops\n", nopCount)
+		}
+
+		if ex, ok := opExec[o]; ok {
+			fmt.Fprintf(b, "%v ", ex.name)
+			for j := 1; j < int(ex.kind.size); j++ {
+				fmt.Fprintf(b, "%v ", m[i+j])
+			}
+			i += int(ex.kind.size)
+			fmt.Fprintf(b, "\n")
+		}
+	}
+	return b.String()
+}
 
 type vm struct {
 	pc  uint
 	r   registers
 	mem memory
+
+	trace bool
 }
 
 func (v *vm) run() error {
@@ -60,44 +104,50 @@ func (v *vm) run() error {
 	for o != halt {
 		ex := opExec[o]
 
-		fmt.Printf("PC: %d Executing: %s\n", v.pc, ex.name)
+		v.log("PC: %d Executing: %s\n", v.pc, ex.name)
 		ex.f(v)
 
-		v.pc += ex.size
+		v.pc += ex.kind.size
 		if v.pc == uint(len(v.mem)) {
 			return fmt.Errorf("finished VM without halting")
 		}
 		o = op(v.mem[v.pc])
 	}
 
-	fmt.Printf("Found halt instruction. Done.\n")
+	v.log("Found halt instruction. Done.\n")
 	return nil
+}
+
+func (v *vm) log(msg string, args ...interface{}) {
+	if v.trace {
+		fmt.Printf(msg, args...)
+	}
 }
 
 func (v *vm) nop() {}
 
 func (v *vm) load() {
 	reg1, reg2 := v.mem[v.pc+1], v.mem[v.pc+2]
-	fmt.Printf("reg1: %v, reg2: %v\n", reg1, reg2)
+	v.log("reg1: %v, reg2: %v\n", reg1, reg2)
 	v.r[reg1] = v.r[reg2]
 }
 
 func (v *vm) loadi() {
 	reg, constant := v.mem[v.pc+1], v.mem[v.pc+2]
-	fmt.Printf("reg: %v, constant: %v\n", reg, constant)
+	v.log("reg: %v, constant: %v\n", reg, constant)
 	v.r[reg] = constant
 }
 
 func (v *vm) add() {
 	dest, reg1, reg2 := v.mem[v.pc+1], v.mem[v.pc+2], v.mem[v.pc+3]
-	fmt.Printf("dest: %v, r1: %v, r2: %v, r[%v]: %v, r[%v]: %v\n",
+	v.log("dest: %v, r1: %v, r2: %v, r[%v]: %v, r[%v]: %v\n",
 		dest, reg1, reg2, reg1, v.r[reg1], reg2, v.r[reg2])
 	v.r[dest] = v.r[reg1] + v.r[reg2]
 }
 
 func (v *vm) addi() {
 	dest, src, constant := v.mem[v.pc+1], v.mem[v.pc+2], v.mem[v.pc+3]
-	fmt.Printf("dest: %v, src: %v, constant: %v\n", dest, src, constant)
+	v.log("dest: %v, src: %v, constant: %v\n", dest, src, constant)
 	v.r[dest] = v.r[src] + constant
 }
 
--- internal/vm_test.go
+++ internal/vm_test.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -10,6 +11,46 @@ func TestInit(t *testing.T) {
 	err := Main()
 	if err != nil {
 		t.Errorf("doSomething() error: %v", err)
+	}
+}
+
+func TestNoZeroLength(t *testing.T) {
+	for _, opex := range opExec {
+		if opex.kind.size == 0 {
+			t.Errorf("Instruction %q is length 0, this is forbidden", opex.name)
+		}
+	}
+}
+
+func TestAssembleString(t *testing.T) {
+	tests := []struct {
+		code []interface{}
+		want string
+	}{
+		{
+			[]interface{}{
+				addi, // r1 = r0 + 1
+				r1,
+				r0,
+				1,
+				halt,
+			},
+			"addi 1 0 1",
+			//			`
+			//addi r1 = r0 + 1 (2 1 0 1)
+			//`,
+		},
+	}
+
+	for _, test := range tests {
+		got, err := assemble(test.code)
+		if err != nil {
+			t.Fatalf("assemble(%v) error: %v", test.code, err)
+		}
+
+		if diff := cmp.Diff(test.want, strings.TrimSpace(got.String())); diff != "" {
+			t.Errorf("assemble diff (-want,+got):\n%v", diff)
+		}
 	}
 }
 
